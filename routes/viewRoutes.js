2e0067781017899a4053631b8761e2b93ad8e1e1
shakib <shakibn350@gmail.com>
2021-02-24 19:20:32 +0600

    TourBd v1

---
+++ routes/viewRoutes.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const viewController=require('./../controllers/viewController');
+const authController=require('./../controllers/authController');
+const bookingController = require('../controllers/bookingController');
+
+const router=express.Router();
+
+
+router.get('/', bookingController.createBookingCheckout,authController.isLogedIn,viewController.getOverview);
+router.get('/tour/:slug',authController.isLogedIn,viewController.getTour);
+router.get('/login',authController.isLogedIn,viewController.getLoginForm);
+router.get('/signup', authController.isLogedIn,viewController.getSignUp);
+router.get('/me', authController.protect, viewController.getAccount);
+
+router.get(
+    '/my-tours',
+    authController.protect,
+    viewController.getMyTours
+);
+  
+router.post('/submit-user-data',authController.protect,viewController.updateUserData);
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err =>{
+  console.log('UncaughtException Shutting down....');
+  console.log(err.name,err.message);
+  process.exit('1');
+});
+dotenv.config({
+  path: './config.env',
+});
+
+const app = require('./app');
+
+//Database Connection Code Start
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+  //mongoose.connect(process.env.DATABASE_LOCAL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => {
+  console.log('Connection Succesfull!!!!');
+});
+//Database Connection Code is end
+
+
+
+//console.log(process.env);
+
+//Start server
+const port = process.env.PORT || 8000;
+const server=app.listen(port, () => {
+  
+  console.log(`This port number is ${port}`);
+});
+
+process.on('unhandledRejection', err =>{
+  console.log('UNHANDLED REJECTION Shutting down!');
+  //console.log(err.name, err.message);
+  server.close(()=>{
+    process.exit(1);
+  });
+});
+
+
