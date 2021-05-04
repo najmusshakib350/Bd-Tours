86351167491e66549520fa0a51280674ecc823c1
shakib <shakibn350@gmail.com>
2021-05-04 10:39:14 +0600

    More Improved Stripe Implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -11,13 +11,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
  const tour = await Tour.findById(req.params.tourId);
   // console.log(tour);
-
-
+  console.log("Hello i am getCheckoutSession function");
+  console.log(req.params.tourId);
+  console.log(tour);
   // 2) Create checkout session
    const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     // success_url:`${req.protocol}://${req.get('host')}/my-tours/?tour=${ req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
-    success_url:`${req.protocol}://${req.get('host')}/my-tours/`,
+    success_url:`${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -25,7 +26,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [` http://127.0.0.1:8000/img/tours/${tour.imageCover}`],
+        images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
@@ -56,7 +57,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
